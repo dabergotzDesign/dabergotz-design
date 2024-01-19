a3a182de4f056ccea49a59e88652554ec0c766c9
dabergotzDesign <otakulp@gmx.de>
2024-01-19 11:33:20 +0100

    update 3.1 - figma
    update with design changes with figma

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,23 +1,134 @@
-/* let acc = document.getElementsByClassName("accordion");
 
-for (let i = 0; i < acc.length; i++) {
+let button_next = document.querySelector(".slider__button-right");
+let button_prev = document.querySelector(".slider__button-left");
+let nextCard = document.querySelector(".next");
+let currentCard = document.querySelector(".current");
+let previousCard = document.querySelector(".previous");
 
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
 
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+button_next.addEventListener("click", () =>{
+  
+  if(currentCard.classList.contains("active") && !nextCard.classList.contains("active")){
 
-  });
-} */
+    currentCard.classList.remove("active");
+    nextCard.classList.add("active");
 
-//MOBILE SIDESCROLL
+    currentCard.style.transform = "translateX(-30%)";
+    nextCard.style.transform = "translateX(-100%)";
 
-let page = document.querySelector(".portfolio__wrapper-mobile");
+  }
+
+  if(!currentCard.classList.contains("active") && previousCard.classList.contains("active")){
+    previousCard.classList.remove("active");
+    currentCard.classList.add("active");
+
+    currentCard.style.transform = "translateX(0)";
+    previousCard.style.transform = "translateX(80%)";
+
+  }
+
+  //right button
+  if(nextCard.classList.contains("active")){
+    button_next.style.display = "none";
+  }
+
+  if(!previousCard.classList.contains("active")){
+    button_prev.style.display = "block";
+  }
+
+
+});
+
+
+button_prev.addEventListener("click", () =>{
+
+  //right card to center, center to left
+  if(currentCard.classList.contains("active") && !previousCard.classList.contains("active")){
+
+    currentCard.classList.remove("active");
+    previousCard.classList.add("active");
+
+    currentCard.style.transform = "translateX(30%)";
+    currentCard.style.zIndex = "1";
+    previousCard.style.transform = "translateX(100%)";
+  }
+
+  //back to normal
+  if(!currentCard.classList.contains("active") && nextCard.classList.contains("active")){
+    nextCard.classList.remove("active");
+    currentCard.classList.add("active");
+
+    currentCard.style.transform = "translateX(0)";
+    nextCard.style.transform = "translateX(-80%)";
+
+  }
+
+  //left button
+  if(previousCard.classList.contains("active")){
+    button_prev.style.display = "none";
+  }
+
+
+  //right button
+  if(!nextCard.classList.contains("active")){
+    button_next.style.display = "block";
+  }
+
+
+});
+
+//mobile
+
+
+//MODAL
+let closeModal = document.querySelector(".close"),
+    card_1 = document.querySelector(".current"),
+    card_2 = document.querySelector(".next"),
+    card_3 = document.querySelector(".previous"),
+    modal_content_1 = document.querySelector(".modal__card-1"),
+    modal_content_2 = document.querySelector(".modal__card-2"),
+    modal_content_3 = document.querySelector(".modal__card-3");
+
+const modal = document.querySelector(".modal__container");
+
+
+closeModal.addEventListener("click", () =>{
+    modal.style.display = "none";
+    modal_content_1.style.display = "none";
+    modal_content_2.style.display = "none";
+    modal_content_3.style.display = "none";
+});
+
+card_1.addEventListener("click", () =>{
+
+  if(card_1.classList.contains("active")){
+    modal.style.display = "flex";
+    modal_content_1.style.display = "block";
+
+  }
+});
+
+card_2.addEventListener("click", () =>{
+
+  if(card_2.classList.contains("active")){
+    modal.style.display = "flex";
+    modal_content_2.style.display = "block";
+
+  }
+});
+
+card_3.addEventListener("click", () =>{
+
+  if(card_3.classList.contains("active")){
+    modal.style.display = "flex";
+    modal_content_3.style.display = "block";
+
+  }
+});
+
+//MOBILE SLIDER
+
+/* let page = document.querySelector(".portfolio__wrapper-mobile");
 
 page.addEventListener("scroll", () =>{
   
@@ -27,7 +138,7 @@ page.addEventListener("scroll", () =>{
   
   document.getElementById("bar").style.width = scrolled + "%";
 
-});
+}); */
 
 
 
