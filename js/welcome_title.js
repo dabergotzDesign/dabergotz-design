19ff7450fb4dff0d78876aaa85497bad21d542ab
dabergotzDesign <otakulp@gmx.de>
2025-05-20 12:09:10 +0200

    welcome mobile update

--- js/welcome_title.js
+++ js/welcome_title.js
@@ -1,21 +1,3 @@
-const mobileMedia = window.matchMedia('(max-width: 450px)');
-
-if(mobileMedia.matches){
-
-  const r_title = new rive.Rive({
-    src: "./rive/ddesign_title.riv",
-    canvas: document.getElementById("canvas"),
-    autoplay: true,
-    artboard: "Innovating_mobile",
-    stateMachines: "State Machine 1",
-    onLoad: () => {
-      r_title.resizeDrawingSurfaceToCanvas();
-    },
-  });
-
-
-}else{
-
  const r_title = new rive.Rive({
   src: "./rive/ddesign_title.riv",
   canvas: document.getElementById("canvas"),
@@ -26,6 +8,3 @@ if(mobileMedia.matches){
     r_title.resizeDrawingSurfaceToCanvas();
   },
 });
-
-
-}
