0ea5f9299711bc4eadb7818bc1fad24090669005
dabergotzDesign <otakulp@gmx.de>
2025-08-30 17:39:13 +0200

    pause bg

--- js/welcome_title.js
+++ js/welcome_title.js
@@ -8,3 +8,21 @@
     r_title.resizeDrawingSurfaceToCanvas();
   },
 });
+
+/* PAUSE BG */
+const pause = document.querySelector(".pauseBG"),
+  pauseIcon = pause.childNodes[0];
+
+
+pause.addEventListener("click", ()=>{
+  const splash_bg = document.querySelector(".ddesign__VM-bg"); 
+  splash_bg.classList.toggle("paused");
+
+  if(pauseIcon.classList.contains("fa-pause")){
+    pauseIcon.classList.remove("fa-pause");
+    pauseIcon.classList.add("fa-play");
+  }else{
+    pauseIcon.classList.remove("fa-play");
+    pauseIcon.classList.add("fa-pause");
+  }
+});
