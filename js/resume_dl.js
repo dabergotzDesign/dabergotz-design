751a37ef51647724628b5f3ca7f65d7b8d4705ca
dabergotzDesign <otakulp@gmx.de>
2026-03-09 10:42:01 +0100

    update resume march

--- js/resume_dl.js
+++ js/resume_dl.js
@@ -1,5 +1,5 @@
 const dl_btn = document.querySelector(".download__cv");
-const fileLink = "https://drive.google.com/uc?export=download&id=1j_IQ_9DovjUGvL_zE70VIbm7XdhsuH8_";
+const fileLink = "https://drive.google.com/uc?export=download&id=12xdF2PgzoapZl5k46vfpaFISVJLaYUxY";
 
 const initTimer = ()=>{
     location.href = fileLink;
