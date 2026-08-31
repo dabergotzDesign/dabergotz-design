365f3035e2251a9b6ccd695deca6a77cb3c59249
dabergotzDesign <otakulp@gmx.de>
2026-08-31 12:08:08 +0200

    blog: design challenge

--- js/slider.js
+++ js/slider.js
@@ -23,31 +23,11 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex-1].style.display = "block";
   slides[slideIndex-1].classList.remove("fade");
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += "active";
 
 
 }
-
-
-/* function autoSlides() {
-
-  let slides = document.getElementsByClassName("slide");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-
-  for (let j = 0; j < dots.length; j++) {
-    dots[j].className = dots[j].className.replace("active", "");    
-  }
-  dots[slideIndex-1].className += "active";
-
-  setTimeout(autoSlides, 5000); // Change image every 5 seconds
-
-} */
